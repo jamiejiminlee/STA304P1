69df04abefd98b228378155e6d5239c0737332e7
Jamie Lee <jamielee@Jamies-MacBook-Pro.local>
2024-09-27 12:55:17 -0400

    updated simulate_data script

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,32 @@
 #### Preamble ####
-# Purpose: Toronto Police Annual Statiscal Report on Reported Crimes
+# Purpose: Simulates data for the Toronto Police Annual Statistical Report - Reported Crimes
 # Author: Jimin Lee 
 # Date: 24 September 2024 
 # Contact: jamiejimin.lee@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: N/A
+# Any other information needed? N/A
 
-
-#### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
-#### Simulate data ####
-# [...ADD CODE HERE...]
+# Set seed for reproducibility
+set.seed(123)
 
+# Number of observations
+n <- 150
 
+# Define variables
+year <- 2014:2023
+geo_division <- c("D11", "D12", "D13", "D14", "D22", "D23", "D31", "D32", "D33",
+                  "D41", "D42", "D43", "D51", "D52", "D53")
+propertycrime_type <- c("Theft", "Break and Enter", "Fraud", "Assault", 
+                        "Trespassing", "Arson", "Other")
 
+# Simulate data using tibble and crossing
+simulated_data <- 
+  tibble(
+    year = 2014:2023,
+    propertycrime_type = sample(propertycrime_type, n, replace = TRUE),
+  )
+
+simulated_data
